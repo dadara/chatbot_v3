c380b59c93a02dbde55cc15c07190e52a1df3b81
Sebastian Czekierski <czekierski@gmail.com>
2014-01-13 19:05:44 +0100

    Drag&Drop Documents + Inventory Open&Close

--- Assets/UIButtonInput.cs
+++ Assets/UIButtonInput.cs
@@ -5,15 +5,28 @@ public class UIButtonInput : MonoBehaviour {
 
 	GameObject panel;
 	Alice alice;
+	UILogic uiLogic;
 	
 	void Start(){
 		panel = GameObject.Find("Panel");
 		alice = panel.GetComponent<Alice>();
+		uiLogic = panel.GetComponent<UILogic>();
 	}
 	void OnPress (bool isPressed)
 	{
-		if (isPressed){
-			alice.inputBot=this.GetComponentInChildren<UILabel>().text;
+		if (isPressed)
+		{				
+			if(this.name.Equals("Input1Button") || this.name == "Input2Button" || this.name == "Input3Button")
+			{
+				alice.inputBot=this.GetComponentInChildren<UILabel>().text;
+			} else if(this.name == "InventoryViewAllButton")
+			{
+				uiLogic.setInventoryIsActive(true);
+				//this.gameObject.SetActive(false);
+			} else if(this.name == "InventoryLargeCloseButton")
+			{
+				uiLogic.setInventoryIsActive(false);
+			}
 		}
 	}
 	
---
+++ Assets/UILogic.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UILogic : MonoBehaviour {
+
+	private bool inventoryIsActive = false;
+
+	GameObject inventoryLargeBackground;
+	GameObject inventoryLargeCloseButton;
+	GameObject inventoryLargeHeader;
+	GameObject inventoryLargeLabel;
+
+	// Use this for initialization
+	void Start () 
+	{
+		inventoryLargeBackground = GameObject.Find("InventoryLargeBackground");
+		inventoryLargeCloseButton = GameObject.Find("InventoryLargeCloseButton");
+		inventoryLargeHeader = GameObject.Find("InventoryLargeHeader");
+		inventoryLargeLabel = GameObject.Find("InventoryLargeLabel");
+
+		inventoryLargeBackground.SetActive(false);
+		inventoryLargeCloseButton.SetActive(false);
+		inventoryLargeHeader.SetActive(false);
+		inventoryLargeLabel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public bool InventoryIsActive {
+		get {
+			return inventoryIsActive;
+		}
+	}
+
+	public void setInventoryIsActive(bool active)
+	{
+		inventoryIsActive = active;
+
+		inventoryLargeBackground.SetActive(active);
+		inventoryLargeCloseButton.SetActive(active);
+		inventoryLargeHeader.SetActive(active);
+		inventoryLargeLabel.SetActive(active);
+	}
+}
