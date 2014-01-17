c809e8a38e39241feef02218a078295363856b19
Sebastian Czekierski <czekierski@gmail.com>
2014-01-17 12:13:26 +0100

    Buttons Disable when Inventory Active

--- Assets/UILogic.cs
+++ Assets/UILogic.cs
@@ -5,24 +5,35 @@ public class UILogic : MonoBehaviour {
 
 	private bool inventoryIsActive = false;
 
+	//Inventory GameObjects that are to be hidden
 	GameObject inventoryLargeBackground;
+	GameObject inventoryLargeBackgroundDark;
 	GameObject inventoryLargeCloseButton;
 	GameObject inventoryLargeHeader;
 	GameObject inventoryLargeLabel;
 	GameObject inventoryLargeScrollBar;
 	GameObject inventoryLargePanel;
 
+	//Other UI Elements
+	GameObject inventoryViewAllButton;
+	GameObject phone;
+
 	// Use this for initialization
 	void Start () 
 	{
 		inventoryLargeBackground = GameObject.Find("InventoryLargeBackground");
+		inventoryLargeBackgroundDark = GameObject.Find("InventoryLargeBackgroundDark");
 		inventoryLargeCloseButton = GameObject.Find("InventoryLargeCloseButton");
 		inventoryLargeHeader = GameObject.Find("InventoryLargeHeader");
 		inventoryLargeLabel = GameObject.Find("InventoryLargeLabel");
 		inventoryLargeScrollBar = GameObject.Find("InventoryLargeScrollBar");
 		inventoryLargePanel = GameObject.Find("InventoryLargePanel");
 
+		inventoryViewAllButton = GameObject.Find("InventoryViewAllButton");
+		phone = GameObject.Find("Phone");
+
 		inventoryLargeBackground.SetActive(false);
+		inventoryLargeBackgroundDark.SetActive(false);
 		inventoryLargeCloseButton.SetActive(false);
 		inventoryLargeHeader.SetActive(false);
 		inventoryLargeLabel.SetActive(false);
@@ -45,11 +56,18 @@ public class UILogic : MonoBehaviour {
 	{
 		inventoryIsActive = active;
 
+		//Enable/Disable Inventory UI Elements
 		inventoryLargeBackground.SetActive(active);
+		inventoryLargeBackgroundDark.SetActive(active);
 		inventoryLargeCloseButton.SetActive(active);
 		inventoryLargeHeader.SetActive(active);
 		inventoryLargeLabel.SetActive(active);
 		inventoryLargeScrollBar.SetActive(active);
 		inventoryLargePanel.SetActive(active);
+
+		//Enable/Disable Phone and InventoryViewAllButton
+		phone.GetComponent<BoxCollider>().enabled = !active;
+		inventoryViewAllButton.GetComponent<BoxCollider>().enabled = !active;
+
 	}
 }
