cc54364082a933572c5cb4e8800ca44268fd5e90
Sebastian Czekierski <czekierski@gmail.com>
2014-02-21 10:56:31 +0100

    Timeshift added

---
+++ Assets/Timeshift.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timeshift : MonoBehaviour 
+{
+	GameObject timeshift;
+	// Use this for initialization
+	void Start () 
+	{
+		timeshift = GameObject.Find("Timeshift");
+		timeshift.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			ActivateTimeShift(true);
+		} else if(Input.GetKeyDown(KeyCode.S))
+		{
+			ActivateTimeShift(false);
+		}
+	}
+
+	public void ActivateTimeShift(bool active)
+	{
+		timeshift.SetActive(active);
+	}
+}
